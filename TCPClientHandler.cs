976efd60863165a9a31d6e918cf728fc4705bf6c
LeeInkyu <123inn9@naver.com>
2018-03-13 15:26:11 +0900

    init

---
+++ TCPClientHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WinAppClient
+{
+    class TCPClientHandler
+    {
+        IPEndPoint clientAddress;
+        IPEndPoint serverAddress;
+
+        TcpClient client;
+
+        NetworkStream networkStream;
+
+        public TCPClientHandler()
+        {
+            clientAddress = new IPEndPoint(IPAddress.Parse("127.0.0.0"), 0); //수정 필요
+            serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.0"), 0); //수정 필요
+
+            client = new TcpClient(clientAddress);
+            client.Connect(serverAddress);
+
+            networkStream = client.GetStream();
+        }
+
+        public TCPClientHandler(string clientIP, string serverIP)
+        {
+            clientAddress = new IPEndPoint(IPAddress.Parse(clientIP), 0); //수정 필요
+            serverAddress = new IPEndPoint(IPAddress.Parse(serverIP), 0); //수정 필요
+
+            client = new TcpClient(clientAddress);
+            client.Connect(serverAddress);
+
+            networkStream = client.GetStream();
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] data = Encoding.Default.GetBytes(message);
+            networkStream.Write(data, 0, data.Length);
+        }
+    }
+}
---
+++ TCPProtocol.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppClient
+{
+    static class TCPProtocolConstants
+    {
+        public const byte NULL = 255;
+
+        public const short REQ_SEQRCH = 0x01;
+        public const short REQ_DATA = 0x02;
+    }
+
+    interface IProtocolUsable
+    {
+        byte[] getBytes();
+        int getSize();
+    }
+
+    class Message : IProtocolUsable
+    {
+        Header header;
+        Body body;
+
+        byte[] bytes;
+
+        public byte[] getBytes()
+        {
+            header.getBytes().CopyTo(bytes, 0);
+            body.getBytes().CopyTo(bytes, header.getSize());
+            return bytes;
+        }
+
+        public int getSize()
+        {
+            return header.getSize() + body.getSize();
+        }
+    }
+
+    class Header : IProtocolUsable
+    {
+        public short MSGTYPE { get; set; }
+        public short MSGSEQ { get; set; }
+        public byte LASTMSG { get; set; }
+
+
+        public byte[] getBytes()
+        {
+            byte[] bytes = new byte[8];
+
+            BitConverter.GetBytes(MSGTYPE).CopyTo(bytes, 0);
+            BitConverter.GetBytes(MSGSEQ).CopyTo(bytes, 2);
+            BitConverter.GetBytes(LASTMSG).CopyTo(bytes, 4);
+            bytes[7] = TCPProtocolConstants.NULL;
+            return bytes;
+
+        }
+
+        public int getSize()
+        {
+            return 8;
+        }
+    }
+
+    class Body : IProtocolUsable
+    {
+        public byte[] DATA { get; set; }
+
+        public byte[] getBytes()
+        {
+            return DATA;
+        }
+
+        public int getSize()
+        {
+            return DATA.Length;
+        }
+    }
+}
