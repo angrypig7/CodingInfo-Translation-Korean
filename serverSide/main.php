48df2c1115b388de30badcc33c64e3a27b3256dd
angrypig7 <angrypig777@gmail.com>
2018-03-31 22:09:20 +0900

    json added

---
+++ serverSide/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>Stack Translated</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: angrypig7
+ * Date: 2018-03-27
+ * Time: 오후 3:42
+ */
+
+
+?>
