7c985148dc8f8e1efde8eb76e5304c988e0d29ab
angrypig7 <angrypig777@gmail.com>
2018-05-10 09:13:15 +0900

    update

---
+++ serverSide/_FTP/db.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$conn = mysqli_connect("localhost", "fishsoup", "adminpassword1db", "fishsoup");
+// "location, username, userpass, database_name"
+?>
